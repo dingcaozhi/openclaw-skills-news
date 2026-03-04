d19d1d1b5778fd9a5782a5995937737832fa86db
dingcaozhi <dingcaozhi@OliverdeMacBook-Pro.local>
2026-03-04 14:39:02 +0800

    重构为纯静态页面，添加定时刷新功能

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://mwc2026-agent-news.netlify.app'
-});
